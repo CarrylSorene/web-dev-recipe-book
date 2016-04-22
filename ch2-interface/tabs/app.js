f1051ac36111c1bff0906a6f7c27b5155bf156e1
Carryl Sorene <carrylsorene@gmail.com>
2016-04-22 17:53:33 +0100

    Switch tab fn

--- ch2-interface/tabs/app.js
+++ ch2-interface/tabs/app.js
@@ -18,3 +18,12 @@ function createTab($container, $content, childSelector) {
   });
   return $newTab;
 }
+
+//first hide everything, then show selected tab
+function switchTab($container, $tab, $content, childSelector) {
+$container.find('childSelector').hide();
+$container.find('ul.tabs > li').removeClass('selected');
+
+$content.slideDown('fast');
+$tab.addClass('selected');
+}
